7a666df90ecabb36666c4ec3d4f73b1c9a90fb45
Hakan <geyikhakan1@gmail.com>
2020-02-24 14:54:29 +0300

    make it better26

--- main.js
+++ main.js
@@ -14,6 +14,11 @@ var app = express()
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use((req,res,next) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://finansappdemo.firebaseapp.com/");
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+})
 
 function getData(){
     request.get('https://www.haremaltin.com/json/all_prices.json',(error,res,body) => {
