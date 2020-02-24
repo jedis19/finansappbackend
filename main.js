2556063d710aa12f4a6e33e8158d7f9d8a0b009d
Hakan <geyikhakan1@gmail.com>
2020-02-24 14:31:16 +0300

    make it better26

--- main.js
+++ main.js
@@ -17,7 +17,6 @@ app.use(bodyParser.json());
 
 app.use((req,res,next) => {
     res.setHeader('Access-Control-Allow-Origin','https://finansappdemo.firebaseapp.com');
-    res.setHeader('Access-Control-Allow-Methods','GET, POST');
     res.setHeader('Access-Control-Allow-Headers',"X-Requested-With,content-type");
     res.setHeader('Access-Control-Allow-Credentials',true);
     next();
