8bcd7b351db21e72cf0714dcf655fb99c4465422
Hakan <geyikhakan1@gmail.com>
2020-02-24 14:05:17 +0300

    make it better23

--- main.js
+++ main.js
@@ -14,13 +14,21 @@ var app = express()
 app.use(cors());
 app.use(bodyParser.json());
 
+
+app.use((req,res,next) => {
+    res.setHeader('Access-Control-Allow-Origin','https://finansappdemo.firebaseapp.com');
+    res.setHeader('Access-Control-Allow-Methos','GET,POST,OPTİONS,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Headers','X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials',true);
+})
+
 function getData(){
     request.get('https://www.haremaltin.com/json/all_prices.json',(error,res,body) => {
         datas = JSON.parse(body);
     })
 }
 
-setInterval(getData,4000)
+setInterval(getData,8000)
 
 mongoose.connect("mongodb://hakan:123456a@ds119072.mlab.com:19072/heroku_52pxwwsv",(error) => {
     if(!error){
