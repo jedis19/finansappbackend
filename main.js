d64e3cf8fcd70b60e9dc5503f1dca4c5e207abbb
Hakan <geyikhakan1@gmail.com>
2020-02-24 14:56:14 +0300

    make it better26

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use((req,res,next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://finansappdemo.firebaseapp.com/");
+    res.setHeader("Access-Control-Allow-Origin", "https://finansappdemo.firebaseapp.com/");
     res.setHeader("Access-Control-Allow-Credentials", true);
     next();
 })
