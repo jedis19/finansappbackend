4df36a296cec66837d82b41582624a7dd0ae9df9
Hakan <geyikhakan1@gmail.com>
2020-02-24 14:27:24 +0300

    make it better23

--- main.js
+++ main.js
@@ -17,6 +17,7 @@ app.use(bodyParser.json());
 
 app.use((req,res,next) => {
     res.setHeader('Access-Control-Allow-Origin','https://finansappdemo.firebaseapp.com');
+    next()
 })
 
 function getData(){
