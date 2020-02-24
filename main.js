13428181d245c8b57de7f31c9fa9f84206c5a0a5
Hakan <geyikhakan1@gmail.com>
2020-02-24 14:12:38 +0300

    make it better24

--- main.js
+++ main.js
@@ -16,10 +16,8 @@ app.use(bodyParser.json());
 
 
 app.use((req,res,next) => {
-    res.setHeader('Access-Control-Allow-Origin','https://finansappdemo.firebaseapp.com');
+    res.setHeader('Access-Control-Allow-Origin','http://finansappdemo.firebaseapp.com');
     res.setHeader('Access-Control-Allow-Methos','GET,POST,OPTİONS,PUT,PATCH,DELETE');
-    res.setHeader('Access-Control-Allow-Headers','X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials',true);
     next();
 })
 
