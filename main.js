950695085ca3ab72e1879184218a491486a37196
Hakan <geyikhakan1@gmail.com>
2020-02-24 14:05:34 +0300

    make it better23

--- main.js
+++ main.js
@@ -20,6 +20,7 @@ app.use((req,res,next) => {
     res.setHeader('Access-Control-Allow-Methos','GET,POST,OPTİONS,PUT,PATCH,DELETE');
     res.setHeader('Access-Control-Allow-Headers','X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials',true);
+    next();
 })
 
 function getData(){
